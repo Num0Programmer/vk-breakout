8dbe5ffedb1cce19a1f2972d4bf5152a8f4ead85
Num0Programmer <num0programmer>
2024-05-23 17:05:52 -0700

    Got enough code to open a window. Decreased the version of winit from 0.30.0 to 0.28.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
-fn main() {
-    println!("Hello, world!");
+use std::sync::Arc;
+use winit::{
+    event::{Event, WindowEvent},
+    event_loop::{ControlFlow, EventLoop},
+    window::WindowBuilder
+};
+
+
+fn main()
+{
+    let event_loop = EventLoop::new();
+    let window = Arc::new(WindowBuilder::new().build(&event_loop).unwrap());
+
+    // vulkan initialization
+
+    event_loop.run(move |event, _, control_flow|
+    {
+        match event
+        {
+            Event::WindowEvent
+            {
+                event: WindowEvent::CloseRequested,
+                ..
+            } => {
+                *control_flow = ControlFlow::Exit;
+            },
+            Event::RedrawEventsCleared => {
+                let image_extent: [u32; 2] = window.inner_size().into();
+            }
+            _ => ()
+        }
+    });
 }
